eeb67924efecfde4bf3b920328b6f32972f720b0
Angela Williams <lambda_school_loaner_97@Lambda-School-Loaner-97s-MacBook-Pro.local>
2020-02-23 19:51:15 -0700

    MVP Completed with stretch

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Navbar from './components/NavBar.js';
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:5000/api/players")
+      .then(res => res.json())
+      .then(players => this.setState({ players }))
+      .catch(err => console.log("Error on Fetch: ", err));
+  }
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar />
+        <h1 data-testid='PlayersHeader'>Players</h1>
+              <div id="PlayerList" data-testid="PlayerList">
+          {this.state.players.map(player => {
+            return (
+              <div key={player.id}>
+                <h2 data-testid='nameOfYourChoice'>{player.name}</h2>
+                <h4>{player.country}</h4>
+                <h5>{player.searches}</h5>
+              </div>
+            );
+          })}
+        </div>
       </header>
     </div>
   );
+}
 }
 
 export default App;
